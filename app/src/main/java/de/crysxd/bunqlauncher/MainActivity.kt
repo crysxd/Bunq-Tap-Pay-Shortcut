70bc983d738edc2eb02717c01f0b660ecb292649
Christian Würthner <cwurthner@virtual-affairs.com>
2020-06-10 10:15:42 +0200

    Resolve intents before starting and show error message if bunq is not isnstalled

--- app/src/main/java/de/crysxd/bunqlauncher/MainActivity.kt
+++ app/src/main/java/de/crysxd/bunqlauncher/MainActivity.kt
@@ -1,16 +1,42 @@
 package de.crysxd.bunqlauncher
 
-import android.content.ComponentName
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
+import android.net.Uri
 import android.os.Bundle
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        startActivity(Intent("com.bunq.android.hce.PAY"))
-        finish()
+        val intent = Intent("com.bunq.android.hce.PAY")
+        safeStartActivity(intent) {
+            AlertDialog.Builder(this)
+                .setMessage(R.string.error_bunq_not_installed)
+                .setPositiveButton(R.string.close) { _, _ ->
+                    finish()
+                }
+                .setNeutralButton(R.string.install_bunq) { _, _ ->
+                    val playStoreIntent = Intent(
+                        Intent.ACTION_VIEW,
+                        Uri.parse("https://play.google.com/store/apps/details?id=com.bunq.android")
+                    )
+                    safeStartActivity(playStoreIntent) {
+                        finish()
+                    }
+                }
+                .show()
+        }
+    }
+
+    private fun safeStartActivity(intent: Intent, fallback: () -> Unit = {}) {
+        if (intent.resolveActivity(packageManager) != null) {
+            startActivity(intent)
+            finish()
+        } else {
+            fallback()
+        }
     }
 }
